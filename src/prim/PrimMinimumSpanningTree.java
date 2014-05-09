8a21eb613992147aad9a9772082c6703dedce09e
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-09 14:46:11 -0500

    Implementation of Prim's Algorithm for MST

---
+++ src/prim/PrimMinimumSpanningTree.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prim;
+
+/**
+ * @author Dany
+ *
+ */
+public class PrimMinimumSpanningTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
