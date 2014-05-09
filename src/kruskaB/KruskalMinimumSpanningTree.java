7e756bd6f6c1bfadeeb88f4dddc3af23fe638fa7
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-09 14:44:03 -0500

    KurskalA algorithm implementation

---
+++ src/kruskaB/KruskalMinimumSpanningTree.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package kruskaB;
+
+/**
+ * @author Dany
+ *
+ */
+public class KruskalMinimumSpanningTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
