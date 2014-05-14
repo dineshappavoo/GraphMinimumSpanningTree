15623f184520fe7cafe6d04723f95b7d52029439
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-13 20:39:33 -0500

    Added the graph construction to Kruskal B MST

--- src/kruskalA/KruskalAMinimumSpanningTree.java
+++ src/kruskalA/KruskalAMinimumSpanningTree.java
@@ -3,17 +3,46 @@
  */
 package kruskalA;
 
+import java.util.Scanner;
+
+import Boruvka.Graph;
+
 /**
  * @author Dany
  *
  */
 public class KruskalAMinimumSpanningTree {
 
-	/**
-	 * @param args
-	 */
+	public static int noOfVertices,noOfEdges;
+	public static Graph<Integer> graph=null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+	}
+	
+	public void constructGraph()
+	{
+		
+		int u, v, w;
+		Scanner scanner=new Scanner(System.in);
+		while(scanner.hasNext())
+		{
+			noOfVertices=scanner.nextInt();
+			noOfEdges=scanner.nextInt();
+
+			graph=new Graph<Integer>(noOfVertices);
+			for(int i=0;i<noOfEdges;i++)
+			{
+				u=scanner.nextInt();
+				v=scanner.nextInt();
+				w=scanner.nextInt();
+				graph.addEdge(u, v, w);
+				//graph.addEdge(v, u, w);
+			}
+			break;
+		}
+		//graph.printGraph();
 
 	}
 
