4d3c446ad2f18ebc4055be43001c6f086160608c
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-13 20:37:59 -0500

    Added the graph construction to boruvka algorithm

--- src/Boruvka/BoruvkaMinimumSpanningTree.java
+++ src/Boruvka/BoruvkaMinimumSpanningTree.java
@@ -3,17 +3,47 @@
  */
 package Boruvka;
 
+import java.util.Scanner;
+
+
 /**
  * @author Dany
  *
  */
 public class BoruvkaMinimumSpanningTree {
 
-	/**
-	 * @param args
-	 */
+	public static int noOfVertices,noOfEdges;
+	public static Graph<Integer> graph=null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		
+		
+	}
+	
+	public void constructGraph()
+	{
+		
+		int u, v, w;
+		Scanner scanner=new Scanner(System.in);
+		while(scanner.hasNext())
+		{
+			noOfVertices=scanner.nextInt();
+			noOfEdges=scanner.nextInt();
+
+			graph=new Graph<Integer>(noOfVertices);
+			for(int i=0;i<noOfEdges;i++)
+			{
+				u=scanner.nextInt();
+				v=scanner.nextInt();
+				w=scanner.nextInt();
+				graph.addEdge(u, v, w);
+				//graph.addEdge(v, u, w);
+			}
+			break;
+		}
+		//graph.printGraph();
 
 	}
 
